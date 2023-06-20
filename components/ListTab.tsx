080d33fdc2e0e4597fa3dd3d524d408e766464e0
Konrad Walas <konradwalas@Konrads-MacBook-Pro.local>
2023-06-20 16:54:54 +0200

    Implement basic fetching list of pokemons

--- components/ListTab.tsx
+++ components/ListTab.tsx
@@ -1,10 +1,36 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 export default function ListTab({navigation}: any) {
+
+  const [pokemons, setPokemons] = useState([]);
+
+  const getPokemons = async () => {
+    try{
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0');
+      setPokemons(response.data.results.map((e:any) => {
+        let obj:any = {}
+        obj.key = e.name;
+        return obj; 
+    }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPokemons();
+    console.log(pokemons);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>This is list tab</Text>
+      <FlatList
+        data={pokemons}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
     </View>
   );
 }
