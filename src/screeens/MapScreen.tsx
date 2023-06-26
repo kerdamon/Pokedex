bc2d77dd267ae463acb9967a0d246afa0806df77
kerdamon <kwalas314@gmail.com>
2023-06-26 16:28:31 +0200

    Add marker press

--- src/screeens/MapScreen.tsx
+++ src/screeens/MapScreen.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import MapView, { LongPressEvent, Marker } from 'react-native-maps';
+import MapView, { LongPressEvent, Marker, MarkerPressEvent } from 'react-native-maps';
 
 export default function MapScreen({navigation}: any) {
   const [markers, setMarkers] = useState<any>([]);
@@ -10,17 +10,22 @@ export default function MapScreen({navigation}: any) {
     setMarkers([
       ...markers,
       {
-        coordinate: e.nativeEvent.coordinate,
         key: JSON.stringify(e.nativeEvent.coordinate.latitude) + JSON.stringify(e.nativeEvent.coordinate.latitude),
+        pokemonData: null,
+        coordinate: e.nativeEvent.coordinate,
       }
     ]);
   }
+
+  const handleMarkerPress = (e:MarkerPressEvent) => {
+    console.log(markers.find(p => p.key === e.nativeEvent.id));
+  }  
 
   return (
     <View style={styles.container}>
       <MapView style={styles.map} onLongPress={e => handleMapPress(e)}>
         {markers.map(m => (
-          <Marker key={m.key} coordinate={m.coordinate}/>
+          <Marker key={m.key} identifier={m.key} coordinate={m.coordinate} onPress={e => handleMarkerPress(e)}/>
         ))}
       </MapView>
     </View>
