c8b215c3d0de5f61fdd0e21606cd8b926278e980
kerdamon <kwalas314@gmail.com>
2023-06-26 15:48:13 +0200

    Implement adding markers upon long click on map

--- src/screeens/MapScreen.tsx
+++ src/screeens/MapScreen.tsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { LongPressEvent, Marker } from 'react-native-maps';
 
 export default function MapScreen({navigation}: any) {
+  const [markers, setMarkers] = useState<any>([]);
+
+  const handleMapPress = (e:LongPressEvent) => {
+    console.log(JSON.stringify(e.nativeEvent.coordinate.latitude) + JSON.stringify(e.nativeEvent.coordinate.latitude));
+    setMarkers([
+      ...markers,
+      {
+        coordinate: e.nativeEvent.coordinate,
+        key: JSON.stringify(e.nativeEvent.coordinate.latitude) + JSON.stringify(e.nativeEvent.coordinate.latitude),
+      }
+    ]);
+  }
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}></MapView>
+      <MapView style={styles.map} onLongPress={e => handleMapPress(e)}>
+        {markers.map(m => (
+          <Marker key={m.key} coordinate={m.coordinate}/>
+        ))}
+      </MapView>
     </View>
   );
 }
