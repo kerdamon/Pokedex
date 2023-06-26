0363d443709fea517b29b6867ebc147dabf36d71
kerdamon <kwalas314@gmail.com>
2023-06-26 16:41:10 +0200

    Add icons for tabs

--- src/containers/TabScreens.tsx
+++ src/containers/TabScreens.tsx
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useSelector } from 'react-redux';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import ListStackScreen from '../screeens/ListStackScreen';
 import FavouriteScreen from "../screeens/FavouriteScreen";
@@ -12,9 +13,34 @@ export const TabScreens = () => {
 
   return (      
     <Tab.Navigator initialRouteName='Home'>
-      <Tab.Screen name="ListStackScreen" component={ListStackScreen} options={{title: 'List of Pokemons'}}/>
-      <Tab.Screen name="FavouriteScreen" component={FavouriteScreen} navigationKey={favouritePokemonName} options={{title: 'Favourite Pokemon'}}/>
-      <Tab.Screen name="MapScreen" component={MapScreen} options={{title: 'Spotted Pokemons'}}/>
+      <Tab.Screen 
+        name="ListStackScreen" 
+        component={ListStackScreen} 
+        options={{
+          title: 'List of Pokemons', 
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="format-list-bulleted" color={color} size={size} />
+          )}}
+      />
+      <Tab.Screen 
+        name="FavouriteScreen" 
+        component={FavouriteScreen} 
+        navigationKey={favouritePokemonName}
+        options={{
+          title: 'Favourite Pokemon', 
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="heart" color={color} size={size} />
+          )}}
+      />
+      <Tab.Screen 
+        name="MapScreen" 
+        component={MapScreen} 
+        options={{
+          title: 'Spotted Pokemons', 
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="map" color={color} size={size} />
+          )}}
+      />
     </Tab.Navigator>
   )
 }
