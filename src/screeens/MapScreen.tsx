a93014a551657818ee4321159a87c5cfcfcf5869
kerdamon <kwalas314@gmail.com>
2023-06-26 15:08:13 +0200

    Add map component

--- src/screeens/MapScreen.tsx
+++ src/screeens/MapScreen.tsx
@@ -1,9 +1,10 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function MapScreen({navigation}: any) {
   return (
     <View style={styles.container}>
-      <Text>This is map tab</Text>
+      <MapView style={styles.map}></MapView>
     </View>
   );
 }
@@ -15,4 +16,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '100%',
+  }
 });
