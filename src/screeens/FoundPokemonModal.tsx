40d40a338e0b4a804349f0b0dbdf8f527cb4849d
kerdamon <kwalas314@gmail.com>
2023-06-29 10:08:09 +0200

    Add pokemon view to found pokemon modal

--- src/screeens/FoundPokemonModal.tsx
+++ src/screeens/FoundPokemonModal.tsx
@@ -1,5 +1,7 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { useQuery } from "@tanstack/react-query";
 
+import { getPokemon } from "../api";
 import PokemonView from "../components/PokemonView";
 import useStorePokemon from "../hooks/useStorePokemon";
 
@@ -7,13 +9,20 @@ export const FoundPokemonModal = ({route, navigation}:any) => {
   const pokemonName = route.params.pokemonName;
   const notes = route.params.notes;
 
+  const {data:pokemonData, isLoading} = useQuery(['pokemon', pokemonName], async () => {
+    return await getPokemon(pokemonName);
+  });
+
   return (
-    <>
-      {/* <PokemonView pokemonData={pokemonData}/> */}
-      <View style={styles.textField}>
-        <Text style={styles.text}>{notes}</Text>
+    <View style={styles.container}>
+      <PokemonView pokemonData={pokemonData} style={styles.pokemonView}/>
+      <View style={styles.notesField}>
+        <Text style={styles.header}>Notes:</Text>
+        <ScrollView style={styles.textField}>
+          <Text style={styles.text}>{notes}</Text>
+        </ScrollView>
       </View>
-    </>
+    </View>
   );
 }
 
@@ -21,9 +30,25 @@ export default FoundPokemonModal;
 
 const styles = StyleSheet.create({
   textField: {
-    flex: 1,
+    flex:1,
+    backgroundColor: 'white',
+    padding: 5
   },
   text: {
 
+  },
+  header: {
+    fontSize: 16,
+    marginBottom: 5
+  },
+  container: {
+    flex:1
+  },
+  pokemonView: {
+    flex: 3
+  },
+  notesField: {
+    flex: 1,
+    margin: 5
   }
 })
